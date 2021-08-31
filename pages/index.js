706d5e442bd88a7ace40f84be406976c187cef38
Zvonimir Perkovic <49445210+perkovz@users.noreply.github.com>
2021-08-30 21:52:27 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -22,26 +22,26 @@ export default function Home() {
 
       <section className="row mb-5">
         <div className="col-12">
-          <h2 className="text-4xl text-center">Customized to fit your needs</h2>
+          <h2 className="text-4xl text-center">Diversified experience</h2>
 
           <div className="row">
-            <div className="col-12 col-sm-6 col-lg-4">
+            <div className="col-12 col-sm-6 col-lg-4 mb-5">
               <div className="card text-center">
                 <div className="card__image">
                   <Image className="" src="/bct_mobile.png" layout="fill" objectFit="contain" alt="Website landing page on mobile view." />
                 </div>
                 <div className="card__title text-xl">Umbraco and Sitecore CMS development</div>
-                <div className="text-base px-2">I started with and built dozens of websites with Umbraco CMS. These days I'm working in Sitecore and making small and medium sites using the JAM stack.</div>
+                <div className="text-base px-2">I started with, built, and maintained dozens of websites built with Umbraco and Sitecore CMS's. My main contribution was the front-end and development of the back office items and components.</div>
               </div>
             </div>
 
-            <div className="col-12 col-sm-6 col-lg-4">
+            <div className="col-12 col-sm-6 col-lg-4 mb-5">
               <div className="card text-center">
                 <div className="card__image">
                   <Image className="" src="/dreambook_hero.png" layout="fill" objectFit="contain" alt="Product landing page" />
                 </div>
-                <div className="card__title text-xl">UX and product design</div>
-                <div className="text-base px-2">I have designed and help develop several custom web applications, creating a cohesive set of visual guidelines focusing on accessibility and scalability.</div>
+                <div className="card__title text-xl">UX, motion and product design</div>
+                <div className="text-base px-2">I have designed and help develop several custom web applications, animations, and style guides, creating cohesive visual guidelines focusing on accessibility and scalability.</div>
               </div>
             </div>
 
@@ -61,25 +61,25 @@ export default function Home() {
 
       <section className="row mb-5">
         <header className="col-12">
-          <h2 className="text-4xl">I can help you with</h2>
-          <p className="text">Projects requiring applied knowledge of website performance, design systems or JAM stack, strong communication, facilitation, and organizational understanding.</p>
+          <h2 className="text-4xl">Current focus</h2>
+          <p className="text">I support projects and teams requiring applied knowledge of website performance, design systems, or JAM stack, and strong communication, facilitation, and organizational understanding.</p>
         </header>
 
         <div className="col-12 col-sm-6 col-md-4">
           <h3 className="text-2xl">01. Web Performance</h3>
-          <p>test</p>
+          <p>Together with accessibility, performance is my first professional passion. Some tasks I enjoy doing are:</p>
           <ul className="bolded-list">
-            <li>Styleguides and design tokens</li>
-            <li>Pattern libraries</li>
-            <li>Workflows and processes</li>
-            <li>Front-end libraries</li>
-            <li>Collaboration and relationships</li>
+            <li>Analysis and interpretation of speed test results</li>
+            <li>Optimizing JS and CSS</li>
+            <li>Optimizing third-party resources and fonts</li>
+            <li>Developing and designing for performance</li>
+            <li>Building performance-based culture</li>
           </ul>
         </div>
 
         <div className="col-12 col-sm-6 col-md-4">
           <h3 className="text-2xl">02. Design Systems</h3>
-          <p>Introduction paragraph</p>
+          <p>Design systems work allows me to use my innate talents to look at the bigger picture, creating and implementing resources, processes, tools, and documentation supporting the design system:</p>
           <ul className="bolded-list">
             <li>Styleguides and design tokens</li>
             <li>Pattern libraries</li>
@@ -91,12 +91,13 @@ export default function Home() {
 
         <div className="col-12 col-sm-6 col-md-4">
           <h3 className="text-2xl">03. JAM Stack</h3>
-          <p>My current interest and focus are JAM Stack Websites. I'm also skilled in the areas bellow:</p>
+          <p>JAM stack development is my newest passion. Currently working on smaller to mid-size projects using these technologies:</p>
           <ul className="bolded-list">
-            <li>Front-end web development</li>
-            <li>Umbraco and Sitecore CMS</li>
             <li>Next.js</li>
-            <li>Vanilla JS and jQuery</li>
+            <li>Eleventy</li>
+            <li>Vercel</li>
+            <li>Netlify</li>
+            <li>Contentful</li>
           </ul>
         </div>
       </section>
@@ -105,7 +106,7 @@ export default function Home() {
         <div className="col-12 mb-5">
           <figure>
             <Image className="" src="/component_architecture.jpg" quality="100" width={1140} height={712} alt="Component architecture in Figma." />
-            <figcaption>How we achieve mirroring effect in Figma.</figcaption>
+            <figcaption>Mirroring effect between Figma styleguide and front-end library.</figcaption>
           </figure>
         </div>
         <div className="col-12 text-center mb-5">

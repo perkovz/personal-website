6f5c2973f4bd045eda2ce41bd6042590c456b57e
Zvonimir Perkovic <zperkovi@gmail.com>
2021-08-31 22:40:57 -0400

    Added Discernible Name to logo.

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <nav>
-      <Link href="/"><a className="logo-link"><div className="logo-img"></div></a></Link>
+      <Link href="/"><a className="logo-link" aria-label="logo"><div className="logo-img"></div></a></Link>
       <Link href="/projects"><a className="nav-link">projects</a></Link>
       {/* <Link href="/articles/"><a className="nav-link">articles</a></Link> */}
       <Link href="/mailto: zperkovi@gmail.com"><a className="nav-link">email</a></Link>
